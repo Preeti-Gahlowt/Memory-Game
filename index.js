ab5b0123a6d0ff4c5d63c78ded53734f0241bb69
PREETI <66962881+Preeti-Gahlowt@users.noreply.github.com>
2020-11-23 15:29:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,9 +13,6 @@ cards.forEach((card)=> card.addEventListener("click",flip))
 
 
 function flip(){
-//console.log("card flipped");
-//console.log(this);
-
 this.classList.add("flip");
 if (!isFlipped) {
   isFlipped=true;
@@ -58,23 +55,25 @@ function reset(){
  firstCard= null;
  secondCard= null;
 }
-// function shuffle(){
-//   cards.forEach((card)=>{
-//     var index= Math.floor(Math.random()*16)
-//     card.style.order= index;
-//   });
-// };
-// const button = document.querySelector(".restart-btn")
-// button.addEventListener("click",()=>{
-//   restart();
-//   shuffle();
-// });
+function shuffle(){
+  cards.forEach((card)=>{
+    var index= Math.floor(Math.random()*16)
+    card.style.order= index;
+  });
+};
+const button = document.querySelector(".restart-btn")
+button.addEventListener("click",()=>{
+  restart();
+  shuffle();
+});
 
-// function restart(){
-// cards.forEach((card)=>card.remove("flip"))
-
-// }
-
+function restart(){
+this.classList.add("flip");
+  if(!isFlipped){
+    isFlipped= false;
+}
+  else{isFLipped=true;}
+}
 (function shuffle(){
   cards.forEach((card)=>{
     var index= Math.floor(Math.random()*16)
